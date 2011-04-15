6d27a7829841902f7b90c460899dc35af165e4d6
Todd Fearn <tfearn@Todd-Fearns-MacBook-Pro.local>
2011-04-15 07:32:14 -0400

    Changed some tabbar icons and views

---
+++ Classes/SettingsViewController.h
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.h
+//  Finster
+//
+//  Created by Todd Fearn on 4/15/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SettingsViewController : UIViewController {
+
+}
+
+@end
