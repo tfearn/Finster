b8df28493ee033371000b533008a0a6ce2282874
Todd Fearn <tfearn@Todd-Fearns-MacBook-Pro.local>
2011-04-23 09:52:46 -0400

    The GetCheckIns and CheckIn REST methods are now pointing to the proper urls

--- Classes/Globals.h
+++ Classes/Globals.h
@@ -19,9 +19,9 @@
 
 // URLs
 #define kUrlTickerLookup			@"http://d.yimg.com/autoc.finance.yahoo.com/autoc?query=%@&callback=YAHOO.Finance.SymbolSuggest.ssCallback"
-#define kUrlGetWall					@"http://www.idata.net/finster/wall3.xml"
-//#define kUrlGetWall				@"https://hostname/getcheckins.cgi?start=1&limit=20&feed=you+wall&type=CheckInTypeIBought&ticker=all"
-#define kUrlPostCheckIn				@"http://www.idata.net/finster/checkin.cgi?type=%@&ticker=%@"
+//#define kUrlGetWall					@"http://www.idata.net/finster/wall3.xml"
+#define kUrlGetWall					@"https://173.203.238.148/getcheckins.cgi?start=1&limit=20&feed=you+wall&type=CheckInTypeIBought&ticker=all"
+#define kUrlPostCheckIn				@"http://173.203.238.148/checkin.cgi?type=%@&ticker=%@"
 
 // Notifications
 #define kNotificationCheckInComplete	@"NotificationCheckInComplete"
